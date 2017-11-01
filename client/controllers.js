cae7b332955d87105c183080259aa6d0af641135
davidmiller963 <30903594+davidmiller963@users.noreply.github.com>
2017-11-01 15:38:50 -0500

    added controller for sendgrid and ngmap

--- client/controllers.js
+++ client/controllers.js
@@ -1,1 +1,22 @@
 angular.module('TruckHunt.controllers', [])
+.controller('ContactUsController', ['$scope', 'ContactForm', function ($scope, ContactForm) {
+    $scope.send = function () {
+        let contact = new ContactForm({
+            from: $scope.email,
+            message: $scope.message
+        });
+        console.log(contact);
+        contact.$save(function () {
+            alert('Thank you for your message. We will get back with you as soon as possible.')
+        }, function (err) {
+            console.log(err)
+        });
+    }
+}])
+.controller('MapController', function(NgMap) {
+    NgMap.getMap().then(function(map) {
+      console.log(map.getCenter());
+      console.log('markers', map.markers);
+      console.log('shapes', map.shapes);
+    });
+  });

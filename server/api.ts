e458088b8a2bc56be70498540fe5ec6aad0f3d30
davidmiller963 <30903594+davidmiller963@users.noreply.github.com>
2017-11-01 13:19:29 -0500

    Added controller and API for sendGrid

--- server/api.ts
+++ server/api.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-
-
-
-
+import contactFormController from './controllers/contact.ctrl';
 
 const router = Router();
+
+router
+  .use('/contactforms', contactFormController)
 
 
 export default router;

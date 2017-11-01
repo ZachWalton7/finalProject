2585ebc150b492b9b1bde107b397f1047f0887f7
ZachWalton7 <31660365+ZachWalton7@users.noreply.github.com>
2017-11-01 15:37:17 -0500

    Factories

--- client/factories.js
+++ client/factories.js
@@ -1,5 +1,19 @@
 angular.module('TruckHunt.factories', [])
-
 .factory('ContactForm', ['$resource', function($resource){
-    return $resource('/api/contactforms/:id', { id: '@id' })
+    return $resource('/api/contactforms/:id', { id: '@id' });
+ }])
+ .factory('Stripe', ['$resource', function($resource){
+     return $resource('/api/stripe/:id', { id: '@id'});
+ }])
+ .factory('Donations', ['$resource', function($resource){
+     return $resource('/api/donations/:id', { id: '@id'});
+ }])
+ .factory('Trucks', ['$resource', function($resource){
+     return $resource('/api/trucks/:id', { id: '@id'});
+ }])
+ .factory('Schedule', ['$resource', function($resource){
+     return $resource('/api/schedule/:id', { id: '@id'});
+ }])
+ .factory('TrucksByCatID', ['$resource', function($resource){
+     return $resource('/api/categories/:id', { id: '@id'});
  }])

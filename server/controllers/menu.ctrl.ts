1c789faa0230f9d60c001d03a922ec5b89e0be96
davidmiller963 <30903594+davidmiller963@users.noreply.github.com>
2017-11-07 14:43:56 -0600

    added items to the menu controllers

--- server/controllers/menu.ctrl.ts
+++ server/controllers/menu.ctrl.ts
@@ -3,6 +3,26 @@ import * as procedures from '../procedures/menu.proc';
 
 const router = Router();
 
+router.get('/:id/:menuid', (req, res) => {
+    procedures.single(req.params.id, req.params.menuid)
+    .then((menu) => {
+        res.send(menu);
+    }).catch((e) => {
+        console.log(e);
+        res.sendStatus(500);
+    });
+});
+
+ router.put('/:id/:menuid', (req, res) => {
+    procedures.update(req.body.idmenus, req.body.item, req.body.cost)
+    .then((item) =>{
+        res.send(item);
+        console.log(item);
+    }).catch((e) => {
+        console.log(e);
+        res.sendStatus(500);
+    })
+});
 
 router.get('/:id', (req, res) => {
     procedures.menu(req.params.id)
@@ -13,15 +33,5 @@ router.get('/:id', (req, res) => {
         res.sendStatus(500);
     });
 });
-
-router.put('/:id', (req, res) => {
-    procedures.update(req.params.idMenus, req.body.item, req.body.cost)
-    .then((item) =>{
-        res.send(item);
-    }).catch((e) => {
-        console.log(e);
-        res.sendStatus(500);
-    })
-})
 
 export default router;

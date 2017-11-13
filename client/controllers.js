0ff10644e5aa2eeb35f3331c2a35763049227dca
davidmiller963 <30903594+davidmiller963@users.noreply.github.com>
2017-11-13 10:32:31 -0600

    fixed redirects for the menu page

--- client/controllers.js
+++ client/controllers.js
@@ -246,9 +246,8 @@ angular.module('TruckHunt.controllers', [])
         $scope.deleteItem = function () {
             console.log('clicked')
             $scope.item.$delete(function () {
-
+                $location.path('/menu');
                 $('#delete-modal').on('hidden.bs.modal', () => {
-                    $location.path('/menu');
                     $scope.$apply();
                 });
 
@@ -269,7 +268,7 @@ angular.module('TruckHunt.controllers', [])
 
                 })
                 i.$save(function (success) {
-                    $location.path('/');
+                    $location.path('/menu');
                 }, function (err) {
                     console.log(err);
                 });
